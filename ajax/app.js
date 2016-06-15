994aefa62d86c730b1fa258348073aaef5ea02fe
Diego Chavez <diegochavezcarro@gmail.com>
2016-06-14 23:48:09 -0300

    ejemplo ajax

---
+++ ajax/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/llamarajax/', function (req, res) {    
+    setTimeout(function() {
+    res.json({ 'message': 'respuesta del servicio' });
+}, 10000);
+});
+
+app.listen(process.env.PORT || 3000);
