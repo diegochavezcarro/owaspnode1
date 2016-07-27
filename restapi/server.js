039ef973df88e2f50cd52c4bb57399652e5b145b
Diego Chavez <diegochavezcarro@gmail.com>
2016-07-27 00:09:55 -0300

    com

--- restapi/server.js
+++ restapi/server.js
@@ -29,7 +29,7 @@ router.get('/', function(req, res) {
 // ----------------------------------------------------
 router.route('/personas')
 
-	// crear unaa persona (POST http://localhost:8080/personas)
+	// crear unaa persona (POST http://localhost:8080/api/personas)
 	.post(function(req, res) {
 		
 		var persona = new Persona();		
