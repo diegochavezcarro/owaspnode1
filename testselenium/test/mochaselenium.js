04ceabd3ffe63b59bbca43fb65fb2a63b8eb19ba
Diego Chavez <diegochavezcarro@gmail.com>
2016-11-21 11:15:00 -0300

    Correcciones para driver version 2.25

--- testselenium/test/mochaselenium.js
+++ testselenium/test/mochaselenium.js
@@ -3,32 +3,40 @@ var assert = require('assert'),
     webdriver = require('selenium-webdriver');
 
 test.describe('Google Search', function () {
-    var driver;
-    test.before(function () {
+    //var driver;
+    /*test.before(function () {
         driver = new webdriver.Builder().
             withCapabilities(webdriver.Capabilities.chrome()).
             build();
     });
     test.after(function () {
         driver.quit();
-    });
+    });*/
 
     test.it('se fija si aparece la palabra en la busqueda', function () {
         this.timeout(7000);
+        var driver = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.chrome()).
+            build();
         driver.get('http://www.google.com');
         var searchBox = driver.findElement(webdriver.By.name('q'));
         searchBox.sendKeys('simple programmer');
         searchBox.getAttribute('value').then(function (value) {
             assert.equal(value, 'simple programmer');
         });
+        driver.quit();
     });
     test.it('se fija si aparece la palabra en el titulo luego de buscar', function () {
+        var driver = new webdriver.Builder().
+            withCapabilities(webdriver.Capabilities.chrome()).
+            build();
         this.timeout(7000);
         driver.get('http://www.google.com');
         var searchBox = driver.findElement(webdriver.By.name('q'));
         searchBox.sendKeys('webdriver');
         driver.findElement(webdriver.By.name('btnG')).click();
         driver.wait(webdriver.until.titleContains('webdriver'), 3000);
+        driver.quit();
     });
 
 });
