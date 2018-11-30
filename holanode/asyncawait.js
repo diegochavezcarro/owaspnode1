ae578f38e67d9c5202921335d53c4eae5c8d4248
Diego Chavez <diegochavezcarro@gmail.com>
2018-11-30 19:35:11 -0300

    asyncawait

--- holanode/asyncawait.js
+++ holanode/asyncawait.js
@@ -1,19 +1,16 @@
-const fs = require('fs');
-const util = require('util');
-
-// Convert fs.readFile into Promise version of same    
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-
-async function leer() {
-    return await readFile('texto.txt', 'utf8');    
-}
-
-// Can't use `await` outside of an async function so you need to chain
-// with then()
-leer().then(data => {
-    console.log(data);
-    return writeFile('nuevo.txt', data, 'utf8');
-}).catch(err => {
-    console.log('error: ' + err);
-});
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    var result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: 'resolved'
+  }
+  
+  asyncCall();
